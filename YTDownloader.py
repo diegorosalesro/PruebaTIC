f83996d6f30fcbf80f4837931f8c77a22b1c5354
Niconavarrov <126114021+Niconavarrov@users.noreply.github.com>
2025-01-29 16:37:51 -0600

    Update YTDownloader.py
    Archivo mejorado imcreiblemente

--- YTDownloader.py
+++ YTDownloader.py
@@ -7,7 +7,7 @@ def descargar_video(url):
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
-    print("Hola soy nique")
+    print("Hola soy una polla")
 
 url = "https://www.youtube.com/watch?v=QdJMgKJH_vg"
 descargar_video(url)
